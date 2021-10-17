ae5020e1573695e87e0ff27e5f2a92aa44278efb
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-17 20:25:16 -0300

    incluindo date picker no input de data

--- Tasks/Controllers/CreateTaskTableViewController.swift
+++ Tasks/Controllers/CreateTaskTableViewController.swift
@@ -7,10 +7,17 @@
 
 import UIKit
 
-class CreateTaskTableViewController: UITableViewController {
+class CreateTaskTableViewController: UITableViewController, UITextFieldDelegate {
 
+    private var datePicker: UIDatePicker = UIDatePicker()
+    private var dateFormatter = DateFormatter()
+    private var selectedIntexPath: IndexPath?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+        datePicker.datePickerMode = .dateAndTime
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -40,10 +47,54 @@ class CreateTaskTableViewController: UITableViewController {
             let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
             return cell
         default:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "DateCell", for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "DateCell", for: indexPath) as! DateTimeTableViewCell
+            cell.textFieldDateTime.inputView = datePicker
+            cell.textFieldDateTime.delegate = self
+            cell.textFieldDateTime.inputAccessoryView = acessoryView()
             return cell
         }
         
     }
 
+    // MARK: - Action Buttons
+    
+    @IBAction func tapSaveButton(_ sender: Any) {
+        print("Task Saved")
+    }
+    
+    // MARK: - UITextFieldDelegate Methods
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let cell = textField.superview?.superview as? DateTimeTableViewCell
+        if let dateCell = cell {
+            self.selectedIntexPath = tableView.indexPath(for: dateCell)
+        }
+    }
+    
+    // MARK: - UIView Functions
+    
+    func acessoryView() -> UIView {
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        
+        let barItemSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(CreateTaskTableViewController.selectDate))
+        
+        toolBar.setItems([barItemSpace, doneButton], animated: true)
+        toolBar.isUserInteractionEnabled = true
+        toolBar.sizeToFit()
+        
+        return toolBar
+    }
+    
+    @objc func selectDate() {
+        if let indexPath = self.selectedIntexPath {
+            let cell = tableView.cellForRow(at: indexPath) as? DateTimeTableViewCell
+            if let dateCell = cell {
+                dateCell.textFieldDateTime.text = dateFormatter.string(from: datePicker.date)
+                self.view.endEditing(true)
+            }
+        }
+    }
+    
 }
